3eef2444881c0bc141d27bcc2d492e29f8533825
vanxuyen0110 <ngoduongvanxuyen0110@gmail.com>
2021-08-25 12:59:03 +0700

    tao nhanh

--- QuanLyNhaHang/TrangChu/models.py
+++ QuanLyNhaHang/TrangChu/models.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 
-import abstract as abstract
+# import abstract as abstract
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
